791aba15e553b5c4a5b1f41dd04f57166a7a3316
HIS <hisjosuapro@gmail.com>
2023-09-12 11:52:32 +0200

    TP6 Partie 1 - Application a développer (24 min)

---
+++ src/Entity/Album.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AlbumRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AlbumRepository::class)]
+class Album
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?int $date = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\ManyToOne(inversedBy: 'albums')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?artiste $artiste = null;
+
+    #[ORM\OneToMany(mappedBy: 'album', targetEntity: Morceau::class)]
+    private Collection $marceaux;
+
+    public function __construct()
+    {
+        $this->marceaux = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDate(): ?int
+    {
+        return $this->date;
+    }
+
+    public function setDate(int $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getArtiste(): ?artiste
+    {
+        return $this->artiste;
+    }
+
+    public function setArtiste(?artiste $artiste): static
+    {
+        $this->artiste = $artiste;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Morceau>
+     */
+    public function getMarceaux(): Collection
+    {
+        return $this->marceaux;
+    }
+
+    public function addMarceaux(Morceau $marceaux): static
+    {
+        if (!$this->marceaux->contains($marceaux)) {
+            $this->marceaux->add($marceaux);
+            $marceaux->setAlbum($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMarceaux(Morceau $marceaux): static
+    {
+        if ($this->marceaux->removeElement($marceaux)) {
+            // set the owning side to null (unless already changed)
+            if ($marceaux->getAlbum() === $this) {
+                $marceaux->setAlbum(null);
+            }
+        }
+
+        return $this;
+    }
+}
